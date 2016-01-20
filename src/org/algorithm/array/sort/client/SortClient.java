b0fb29be045286ff139390e50f4053e132b73849
William-Hai <return_zero0@163.com>
2016-01-20 09:58:34 +0800

    添加了插入与选择排序

--- src/org/algorithm/array/sort/client/SortClient.java
+++ src/org/algorithm/array/sort/client/SortClient.java
@@ -1,6 +1,6 @@
 package org.algorithm.array.sort.client;
 
-import org.algorithm.array.sort.impl.QuickSort;
+import org.algorithm.array.sort.impl.SelectInsert;
 import org.algorithm.array.sort.interf.Sortable;
 import org.utils.naga.containers.ArrayUtils;
 
@@ -8,7 +8,7 @@ public class SortClient {
 
     public static void main(String[] args) {
         int[] array = {34, 23, 98, 76, 56, 54, 90, 12, 34, 65, 45, 78};
-        Sortable sortable = new QuickSort();
+        Sortable sortable = new SelectInsert();
         array = sortable.sort(array);
         
         ArrayUtils.show(array);
