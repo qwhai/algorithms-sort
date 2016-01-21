f2f23feb2dadb8a5b989f5b0cf0d075a305e6625
William-Hai <return_zero0@163.com>
2016-01-21 14:32:38 +0800

    添加希尔排序

--- src/org/algorithm/array/sort/client/SortClient.java
+++ src/org/algorithm/array/sort/client/SortClient.java
@@ -1,6 +1,6 @@
 package org.algorithm.array.sort.client;
 
-import org.algorithm.array.sort.impl.MergeSort;
+import org.algorithm.array.sort.impl.ShellSort;
 import org.algorithm.array.sort.interf.Sortable;
 import org.utils.naga.containers.ArrayUtils;
 
@@ -9,7 +9,7 @@ public class SortClient {
     public static void main(String[] args) {
         int[] array = {34, 23, 76, 56, 54, 12, 34, 65, 45, 9, 8, 7, 6, 5};
 //        int[] array = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        Sortable sortable = new MergeSort();
+        Sortable sortable = new ShellSort();
         array = sortable.sort(array);
         
         ArrayUtils.show(array);
--- src/org/algorithm/array/sort/impl/ShellSort.java
+++ src/org/algorithm/array/sort/impl/ShellSort.java
@@ -4,7 +4,7 @@ import org.algorithm.array.sort.interf.Sortable;
 
 /**
  * <p>
- * 希尔排序
+ * 希尔排序算法
  * </p>
  * 2016年1月20日
  * 
@@ -16,8 +16,40 @@ public class ShellSort implements Sortable {
 
     @Override
     public int[] sort(int[] array) {
-        // TODO Auto-generated method stub
-        return null;
+        if (array == null) {
+            return null;
+        }
+        
+        core(array);
+        
+        return array;
     }
 
+    // 希尔排序的核心模块
+    private void core(int[] array) {
+        int arrayLength = array.length;
+        int step = arrayLength / 2;
+        do {
+            for (int i = 0; i < step; i++) {
+                insert(array, i, step);
+            }
+            step /= 2;
+        } while (step > 0);
+    }
+    
+    // 希尔排序的直接插入排序
+    private void insert(int[] array, int offset, int step) {
+        int groupCount = array.length / step + (array.length % step > offset ? 1 : 0);
+        for (int i = 1; i < groupCount; i++) {
+            int nextIndex = offset + step * i;
+            int waitInsert = array[nextIndex];
+            
+            while(nextIndex - step >= 0 && waitInsert < array[nextIndex - step]) {
+                array[nextIndex] = array[nextIndex - step];
+                nextIndex -= step;
+            }
+            
+            array[nextIndex] = waitInsert;
+        }
+    }
 }
