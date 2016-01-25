678efa6ee81bbeb0d16181da9dc00e66269b5d8b
William-Hai <return_zero0@163.com>
2016-01-25 12:58:36 +0800

    modify

--- src/org/algorithm/array/sort/impl/OddEvenSort.java
+++ src/org/algorithm/array/sort/impl/OddEvenSort.java
@@ -6,13 +6,16 @@ import org.algorithm.array.sort.interf.Sortable;
  * <p>
  * 奇偶排序算法
  * </p>
+ * <p>
+ * https://en.wikipedia.org/wiki/Odd%E2%80%93even_sort
+ * </p>
  * 2016年1月21日
  * 
  * @author <a href="http://weibo.com/u/5131020927">Q-WHai</a>
  * @see <a href="http://blog.csdn.net/lemon_tree12138">http://blog.csdn.net/lemon_tree12138</a>
  * @version 0.1.1
  */
-public class ParitySort implements Sortable {
+public class OddEvenSort implements Sortable {
 
     @Override
     public int[] sort(int[] array) {
